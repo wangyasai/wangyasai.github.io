402aa3efe6d640cf1b16b73f1283e12174235a21
yasai wang <yasaiwang@yasaideMacBook-Pro.local>
2019-10-30 23:17:18 +0800

    fix textsize

--- js/sketch.js
+++ js/sketch.js
@@ -14,11 +14,8 @@ var engine,world;
 var circles = [];
 var mConstraint;
 
-
-
-var ground,groundL,groundR;
-let t = ["p5.js","Processing","Ai","Ps","Data","Design","Indesign","Matter.js","Yasai","AE","QGis","D3.js","THREE.js","BootStrap","Html","css"];
-let r= [6,7,7,6,6,3,2,3,5,3,4,1,1,2,4,3];
+let t = ["p5.js","Processing","Ai","Ps","Data","Design","Indesign","Matter.js","AE","QGis","D3.js","THREE.js","BootStrap","Html","css"];
+let r= [6,7,7,6,6,3,2,3,3,4,1,1,2,4,3];
 
 
 function setup () {
@@ -35,7 +32,7 @@ function setup () {
 	groundT = Bodies.rectangle( width/2, -3000,width*3,100,options);
 	World.add(world, [ground,groundL,groundR,groundT]);
 	for(var i = 0 ; i < t.length; i++){
-		var radius = map(r[i],0,10,30,70);
+		var radius = map(r[i],0,10,20,70);
 		var n = map(windowWidth,400,2000,1,2.5);
 		circles.push(new Circle(random(width),random(-200,-2000), radius*n,t[i]));
 	}
@@ -56,23 +53,33 @@ function setup () {
 
 function draw () {
 	pixelDensity(2);
-	background ('#FC8AA3');
+	background ('#445FAC');
 	var percent = norm(sin(PI/2+frameCount/100), -1, 1);
-	var between = lerpColor(color('#97E4FF'),color('#FC8AA3'), percent);
+	var between = lerpColor(color('#eedacf'),color('#f3f3f3'), percent);
 	fill(between);
 	noStroke();
 	rect(0,0,width,height);
 
-	fill(255,100);
+	fill('#ea7689');
 	noStroke();
-	var size = map(windowWidth,375,2000,22,30);
+	var size = map(windowWidth,375,2000,14,22);
 	textSize(size);
 	textAlign(CENTER);
-	text("HI, 我是亚赛", width/2, height*0.4 );
-	text("数据可视化设计师", width/2, height*0.45);
-	text("设计工具制造玩家", width/2, height*0.5);
+	textSize(size*2.5);
+	// text("HI, I'm Yasai", width/2, height*0.4 );
+	// textSize(20);
+	// textAlign(LEFT);
+	// text("I'm a data visualization designer who is curiosity of data, design and coding. I create elegant and creative vis to tell story. Besides, I have been made ten effective and effient design tools. Have fun：）", width/2-200, height*0.42,size*20,900);
 
-	for(var i = 0; i < t.length; i++){
+	text("HI, 我是亚赛", width/2, height*0.4 );
+	textSize(size);
+	textAlign(CENTER);
+	text("我是一名数据可视化设计师", width/2, height*0.45);
+	text("喜欢用编程和设计去创造优雅并美观的可视化作品", width/2, height*0.48);
+	text("也是一名设计工具制造玩家", width/2, height*0.51);
+	text("误打误撞产出10款提高效率的设计小工具", width/2, height*0.54);
+
+	for(var i = 0; i < t.length; i++){ 
 		circles[i].show();
 	}   
 
